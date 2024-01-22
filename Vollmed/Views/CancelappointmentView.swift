da602cdd88f29dc8e7b04796d1ac7b2d0661b5a0
Quellenni Reis <quellenni.reis@Quellennis-MacBook-Air.local>
2024-01-22 18:18:42 -0300

    Implements PATCH and DELETE requests

---
+++ Vollmed/Views/CancelappointmentView.swift
@@ -0,0 +1,18 @@
+//
+//  CancelappointmentView.swift
+//  Vollmed
+//
+//  Created by Quellenni Reis on 20/01/24.
+//
+
+import SwiftUI
+
+struct CancelappointmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CancelappointmentView()
+}
